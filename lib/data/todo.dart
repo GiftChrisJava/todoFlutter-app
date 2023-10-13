687307a23c0e9663015a839715e2cf1f3357f15c
GiftChrisJava <chrisjava77@gmail.com>
2023-10-13 07:39:48 +0100

    started to do flutter app

---
+++ lib/data/todo.dart
@@ -0,0 +1,24 @@
+class Todo {
+  final String title;
+  final String subtitle;
+  bool isDone;
+
+// constructor
+  Todo({
+    this.title = "",
+    this.subtitle = "",
+    this.isDone = false,
+  });
+
+  Todo copyWith({
+    String? title,
+    String? subtitle,
+    bool? isDone,
+  }) {
+    return Todo(
+      title: title ?? this.title,
+      subtitle: subtitle ?? this.subtitle,
+      isDone: isDone ?? this.isDone,
+    );
+  }
+}
